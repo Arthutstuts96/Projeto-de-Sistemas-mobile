ae922fed5315537de6edb41d0ca3dc9d9bce7849
GFernandesZ <g.f.z2012gfz@gmail.com>
2025-04-13 18:06:51 -0300

    Tornando a interface mais completa

--- lib/ui/login_screen.dart
+++ lib/ui/login_screen.dart
@@ -6,57 +6,107 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
+      body: Column(
         children: [
-          Image.asset("assets/images/circle.png"),
-          Align(
-            alignment: Alignment(0.0, -0.8),
-            child: Text(
-              "Bem vindo ao Traz Aí",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 50,
-                fontWeight: FontWeight.bold,
-                fontFamily: "Lato",
-                color: Color.fromARGB(255, 90, 86, 86),
-              ),
-            ),
+          Image.asset(
+            "assets/images/login_image.png",
+            fit: BoxFit.cover, // Ajusta a imagem
+            width: double.infinity, // Faz a imagem ocupar toda a largura
           ),
-          Padding(
-            padding: const EdgeInsets.all(32), // Ajuste o valor do padding aqui
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(height: 32),
-                const Text(
-                  "Login",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 32),
+          Expanded(
+            child: Container(
+              width:
+                  double.infinity, // Faz a box ocupar toda a largura disponível
+              padding: const EdgeInsets.all(16), // Espaçamento interno da box
+              decoration: BoxDecoration(
+                color: Colors.white, // Cor de fundo da box
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(
+                    16,
+                  ), // Apenas o topo esquerdo arredondado
+                  topRight: Radius.circular(
+                    16,
+                  ), // Apenas o topo direito arredondado
                 ),
-                const SizedBox(height: 32), // Espaçamento entre os widgets
-                TextFormField(
-                  decoration: InputDecoration(label: Text("Email ou Usuário")),
-                ),
-                const SizedBox(height: 32), // Espaçamento entre os campos
-                TextFormField(
-                  decoration: InputDecoration(label: Text("Senha")),
-                  obscureText: true,
-                ),
-                const SizedBox(height: 32), // Espaçamento entre os campos
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushReplacementNamed(context, "home");
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: WidgetStatePropertyAll(Color(0xFF6DB1FF)),
-                    minimumSize: WidgetStatePropertyAll(Size(400, 50)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26, // Sombra leve
+                    blurRadius: 8,
+                    offset: Offset(0, 4),
                   ),
-                  child: const Text(
-                    "Entrar",
-                    style: TextStyle(color: Colors.white),
-                  ),
+                ],
+              ),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Bem vindo ao Traz Aí",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: "Poppins",
+                        color: Color.fromARGB(255, 239, 159, 10),
+                      ),
+                    ),
+                    const SizedBox(height: 32),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text("E-mail"),
+                    ), // Espaçamento entre o texto e os campos
+                    TextFormField(),
+                    const SizedBox(height: 32),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text("Senha"),
+                    ), // Espaçamento entre os campos
+                    TextFormField(obscureText: true),
+                    const SizedBox(
+                      height: 32,
+                    ), // Espaçamento entre o campo e o botão
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.pushReplacementNamed(context, "home");
+                          },
+                          style: ButtonStyle(
+                            backgroundColor: WidgetStatePropertyAll(
+                              const Color.fromARGB(255, 239, 159, 10),
+                            ),
+                            minimumSize: WidgetStatePropertyAll(
+                              const Size(185, 50),
+                            ),
+                          ),
+                          child: const Text(
+                            "Entrar",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.pushReplacementNamed(context, "cadastro");
+                          },
+                          style: ButtonStyle(
+                            backgroundColor: WidgetStatePropertyAll(
+                              const Color.fromARGB(255, 226, 251, 0),
+                            ),
+                            minimumSize: WidgetStatePropertyAll(
+                              const Size(185, 50),
+                            ),
+                          ),
+                          child: const Text(
+                            "Cadastro",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
         ],
